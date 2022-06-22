bbfbeabd9cc1ecca7599c4e851f594c4ac472374
Leonardo Garcia <leonardogirotogarcia@gmail.com>
2022-06-22 18:31:34 -0300

    Segregação do diretorio error-404

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,31 +4,25 @@ import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { Error404Component } from './error-404/error-404.component';
 import { CourseModule } from './courses/course.module';
 import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    Error404Component,  
+    AppComponent
   ],
   imports: [
-    CoreModule,
     BrowserModule,
     HttpClientModule,
     CourseModule,
+    CoreModule,
     RouterModule.forRoot([ // Serão usadas as rotas padrões do Angular para essa aplicação. Elas servem para fazer a refência dos componentes via URL
-            { // Objeto de rota do path base da aplicação
-        path: '', // ROTA PADRÃO. Path vazio faz referência a raíz da aplicação.
-        redirectTo: 'courses', // Redirecionará para /courses na URL.
-        pathMatch: 'full'
-      },
-      {
-        path: '**', // ROTA PADRÃO. Quando não encontrar uma rota, será redirecionado para ele.
-        component: Error404Component
-      }
-    ])
+    { // Objeto de rota do path base da aplicação
+      path: '', // ROTA PADRÃO. Path vazio faz referência a raíz da aplicação.
+      redirectTo: 'courses', // Redirecionará para /courses na URL.
+      pathMatch: 'full'
+    }
+  ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { NavBarComponent } from "./component/nav-bar/navBar.component";
+import { Error404Component } from "./component/error-404/error-404.component";
 
 @NgModule ({
     declarations: [
-        NavBarComponent
+        NavBarComponent,
+        Error404Component
     ],
     imports: [
-        RouterModule
+        RouterModule.forChild([
+            {
+                path: '**', // ROTA PADRÃO. Quando não encontrar uma rota, será redirecionado para ele.
+                component: Error404Component
+            }
+        ])
     ],
     exports: [
         NavBarComponent
